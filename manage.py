908fdc0e8ef2391f1248d5ef753a564fedbc31de
JuanEchague <loxar240@gmail.com>
2021-07-04 17:52:52 -0300

    version1
    Css

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Trabajo_Practico.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jkaa.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
