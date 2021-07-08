b96fa8376b640350c8376b03b1560a274ee1fefb
JuanEchague <loxar240@gmail.com>
2021-07-07 23:02:14 -0300

    commit on main

--- Jkaa/pags/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PagsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pags'
--- Jkaa/pags/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('pag1', views.pag1, name="pag1"),
-    path('pag2', views.pag2, name="pag2"),
-    path('pag3', views.pag3, name="pag3"),
-]
